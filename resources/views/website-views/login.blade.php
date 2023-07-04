879b2c5b0fa3daa8ec187514b06aabe55c7a77f7
Abdulaziz Ba Haj <abdulazizbahajworks@gmail.com>
2023-07-04 09:54:06 +0300

    login page completed

--- resources/views/website-views/login.blade.php
+++ resources/views/website-views/login.blade.php
@@ -14,16 +14,23 @@
             <div class="col-md-6 p-0 m-0">
                 <div class="login d-flex align-items-center form-div">
                     <div class="container">
-                        <div class="hr-line mb-3"></div>
-                        <h1 class="header-text">Access Your Account</h1>
-                        <p class="sm-text">Provide your mobile number and use OTP to login into your account securely.</p>
-                        <div class="container white-form-div">
+                        <div class="container text-half">
+                            <div class="hr-line mb-3"></div>
+                            <h1 class="header-text">Access Your Account</h1>
+                            <p class="sm-text">Provide your mobile number and use OTP to login into your account securely.
+                            </p>
+                        </div>
+                        <div class="container white-form-div d-flex flex-column">
                             <p for="" class="label-text mb-3">Let’s Start Now 🔥</p>
                             <div class="input-group mb-3">
                                 <span class="input-group-text border border-end-0 bg-transparent"><i
                                         class="bi bi-telephone"></i></span>
                                 <span class="input-group-text border border-start-0 border-end-0 bg-transparent">+966</span>
                                 <input type="text" class="form-control" placeholder="Phone Number">
+                            </div>
+                            <div class="mt-auto text-center">
+                                <button class="btn btn-primary btn-style" type="button">Login</button>
+                                <p class="sm-text-rights mt-3">© MELI All rights reserved.</p>
                             </div>
                         </div>
                     </div>
