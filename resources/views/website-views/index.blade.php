a7150994588b2f3f45e52571ed2ab40d87e37745
Abdulaziz Ba Haj <abdulazizbahajworks@gmail.com>
2023-07-09 14:34:56 +0300

    added footer

--- resources/views/website-views/index.blade.php
+++ resources/views/website-views/index.blade.php
@@ -611,7 +611,8 @@
             <div class="container py-5 py-xl-6 px-5 text-center q-div">
                 <div class="container">
                     <h1 class="q-title">Have questions or need assistance?</h1>
-                    <p class="mt-5 q-sm-text"> Contact our dedicated team at MELI for personalized guidance and support. We are here to help you
+                    <p class="mt-5 q-sm-text"> Contact our dedicated team at MELI for personalized guidance and support. We
+                        are here to help you
                         make
                         informed decisions about your logistics education journey. Reach out to us today and take the first
                         step
@@ -622,6 +623,43 @@
             </div>
         </div>
     </section>
+
+    <div class="fluid-container gradient-bg">
+        <div class="container">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
+                <div class="col-lg-4 d-flex align-items-center">
+                    <img src="assets\images\red-icon.png" alt="" class="img-fluid" style="max-width: 100px">
+                </div>
+
+                <div class="nav col-lg-4 justify-content-end">
+                    <span class="mb-3 mb-lg-0 text-muted">Privacy Policy</span>
+                </div>
+            </footer>
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
+                <div class="col-lg-4 d-flex align-items-center">
+                    <span class="mb-3 mb-lg-0 text-muted">© MELI all rights reserved.</span>
+                </div>
+
+
+                <ul class="nav col-lg-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3">
+                        <button class="footer-icon"><i class="bi bi-twitter"></i></button>
+                    </li>
+                    <li class="ms-3">
+                        <button class="footer-icon"><i class="bi bi-instagram"></i></button>
+                    </li>
+                    <li class="ms-3">
+                        <button class="footer-icon"><i class="bi bi-facebook"></i></button>
+                    </li>
+                    <li class="ms-3">
+                        <button class="footer-icon"><i class="bi bi-youtube"></i></button>
+                    </li>
+                </ul>
+
+            </footer>
+        </div>
+    </div>
+
 @section('scripts')
     <script src="assets/js/website/home.js"></script>
 @endsection
