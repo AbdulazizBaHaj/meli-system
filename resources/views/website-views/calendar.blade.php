12e6c46c83c7b2efb0b0080927d631eef4ecf9af
Abdulaziz Ba Haj <abdulazizbahajworks@gmail.com>
2023-07-13 11:51:25 +0300

    contact box breakdown

--- resources/views/website-views/calendar.blade.php
+++ resources/views/website-views/calendar.blade.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="assets/css/websiteCss/_navbar.css">
     <link rel="stylesheet" href="assets/css/websiteCss/_footer.css">
     <link rel="stylesheet" href="assets/css/websiteCss/_breadcrumb.css">
+    <link rel="stylesheet" href="assets/css/websiteCss/_contact-box.css">
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
 @endsection
 
@@ -163,21 +164,7 @@
 
 
     <section class="m-auto section-style">
-        <div class="container py-5 py-xl-6 px-5 text-center">
-            <div class="container py-5 py-xl-6 px-5 text-center q-div">
-                <div class="container">
-                    <h1 class="q-title">Have questions or need assistance?</h1>
-                    <p class="mt-5 q-sm-text"> Contact our dedicated team at MELI for personalized guidance and support. We
-                        are here to help you
-                        make
-                        informed decisions about your logistics education journey. Reach out to us today and take the first
-                        step
-                        towards unlocking your potential in the world of logistics</p>
-                    <button class="btn q-btn text-center mt-5" type="button">Contact
-                        Us</button>
-                </div>
-            </div>
-        </div>
+        @include('website-views._contact-box')
     </section>
 
     @include('website-views._footer')
