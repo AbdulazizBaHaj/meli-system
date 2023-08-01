b17c9f43a53209554591648d030197825a421d60
Abdulaziz Ba Haj <abdulazizbahajworks@gmail.com>
2023-08-01 16:33:36 +0300

    folder changes

---
+++ student-portal/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.sysadmin-layout')
+
+@section('styles')
+    {{-- <link rel="stylesheet" href="{{ URL::asset('assets/css/websiteCss/admission-apply.css') }}"> --}}
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/websiteCss/_navbar.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/websiteCss/_footer.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/websiteCss/_breadcrumb.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/websiteCss/_contact-box.css') }}">
+@endsection
+
+@section('title', 'System Dashboard')
+
+@section('body')
+<div class="container">
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse sequi blanditiis veritatis id, quas quae? Deserunt, perspiciatis. Minima, voluptatem. Odio suscipit ullam, officia fugit consequatur harum nemo! Beatae, quisquam debitis.</p>
+</div>
+@endsection
